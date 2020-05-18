474c00e919a13a9d13a30c0fd877eefb42d6669a
KaustuvBasak26 <kbasak5190@gmail.com>
2020-05-18 16:31:37 +0530

    enhanced isLeap year to return false is a year is divisible by 100 but not by 400 and true if it is divisible by 400; the test breaks if years like 1700 and 2100 is passed if our assertion is to return true and assets true if a year like 2000 is passed

--- TDDExample/src/com/xminds/main/LeapYear.java
+++ TDDExample/src/com/xminds/main/LeapYear.java
@@ -2,8 +2,11 @@ package com.xminds.main;
 
 public class LeapYear {
 	public static boolean isLeap(int year){
+		if(year%100 == 0 && year%400 !=0){
+			return false;		//added rule 2: if year is divisible by 100 but not divisible by 400 then it is not a leap year 
+		}
 		if(year%400 == 0){
-			return true; 		//added rule 1: if year is divisible by 400 the it is leap
+			return true; 		//added rule 1: if year is divisible by 400 then it is leap
 		}
 		return false;
 	}
--- TDDExample/src/com/xminds/test/TestJUnit1.java
+++ TDDExample/src/com/xminds/test/TestJUnit1.java
@@ -8,6 +8,6 @@ import com.xminds.main.LeapYear;
 public class TestJUnit1 {
 	@Test
 	public void test1(){
-		Assert.assertEquals(true, LeapYear.isLeap(2000));
+		Assert.assertEquals(true, LeapYear.isLeap(2100));
 	}
 }
