4eb6bd08b38f5ac1cb3bdf567b8a5c66365d80c4
KaustuvBasak26 <kbasak5190@gmail.com>
2020-05-18 16:42:12 +0530

    added rule 3 to isLeap which returns true if year divisible by 4 and not divisible by 100 is passed or a year divisible by 400 is passed and returns false if a year divisible by 100 but not by 400 is passed, the test TestJUnit1 asserts true when years like 2000, 2004, 2012, 2016 are passed but false when years like 1700, 1800, 1900 or 2100 are passed

--- TDDExample/src/com/xminds/main/LeapYear.java
+++ TDDExample/src/com/xminds/main/LeapYear.java
@@ -2,9 +2,13 @@ package com.xminds.main;
 
 public class LeapYear {
 	public static boolean isLeap(int year){
+		
+		if(year%4 == 0 && year%100 !=0){
+			return true;		//added rule 3: if year is divisible by 4 but not divisible by 100 then it is a leap year 
+		}else
 		if(year%100 == 0 && year%400 !=0){
 			return false;		//added rule 2: if year is divisible by 100 but not divisible by 400 then it is not a leap year 
-		}
+		}else
 		if(year%400 == 0){
 			return true; 		//added rule 1: if year is divisible by 400 then it is leap
 		}
